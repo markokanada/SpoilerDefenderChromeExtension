1fae63666c6ded9cfacb14755d1f032c05d85db5
markokanada <39034479+markokanada@users.noreply.github.com>
2021-10-12 14:14:27 +0200

    V.0.2

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 chrome.action.onClicked.addListener(function() {
   //chrome.tabs.create({ url: 'https://techtabor.agondolkodasorome.hu' });
-  chrome.scripting.executeScript({function(){
-    alert("Test");
-  }})
+  //const tabId = getTabId();
+  chrome.scripting.executeScript(
+    {
+      target: {tabId: tabId},
+      files: ['content-script.js'],
+    });
 });
