dc3301cbe5d0897012dd4193e7f664e17338383a
markokanada <39034479+markokanada@users.noreply.github.com>
2021-10-12 15:53:37 +0200

    V.0.9

--- content-script.js
+++ content-script.js
@@ -1,1 +1,8 @@
-alert("Haloooo");
+var Spoiler_Words = "Lorem";
+var Spoiler_Change_Words = "Vigyázz Spoiler harap!";
+
+var Elements = document.getElementsByTagName("h1");
+
+for( let a=0; a < Elements.lenght; a++ ){
+    Elements[a].innerHTML=Elements[a].innerHTML.replace(Spoiler_Words, Spoiler_Change_Words);
+}
