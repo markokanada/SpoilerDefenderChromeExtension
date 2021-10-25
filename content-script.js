35201929a9bf56a6ed7b277adb3d27ef01bea080
B4lint0413 <69457665+B4lint0413@users.noreply.github.com>
2021-10-25 10:46:25 +0200

    V 1.1
    From now you can give more spoiler words.

--- content-script.js
+++ content-script.js
@@ -1,10 +1,20 @@
-var Spoiler_Words = prompt("Add meg, hogy mit nem szeretnél látni!");
+var Spoiler_Words = [];
 var Spoiler_Change_Words = "Vigyázz Spoiler harap!";
 var elements = document.querySelectorAll("p,h1,h2,h3,h4,h5,h6,li");
 
-for( let a=0; a < elements.length; a++ ){
-    if (elements[a].innerHTML.search(Spoiler_Words) != -1 && Spoiler_Words.length > 1) {
-        elements[a].innerHTML = Spoiler_Change_Words;
-        elements[a].style.backgroundColor = "red";
+while (true) {
+    let current = prompt("Add meg, hogy mit nem szeretnél látni! Ne adj meg semmit, ha nem szeretnél több kulcsszót megadni!");
+    if (current == "") {
+        break;
+    }else{
+        Spoiler_Words.push(current)    
+    }
+}
+for (let i = 0; i < Spoiler_Words.length; i++) {
+    for( let a=0; a < elements.length; a++ ){
+        if (elements[a].innerHTML.search(Spoiler_Words[i]) != -1 && Spoiler_Words.length > 1) {
+            elements[a].innerHTML = Spoiler_Change_Words;
+            elements[a].style.backgroundColor = "red";
+        }
     }
 }
